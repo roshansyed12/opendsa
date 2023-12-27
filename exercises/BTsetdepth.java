5f706134e57d0a2375c2c9716dbd8534e2cdde49
roshansyed12 <154960598+roshansyed12@users.noreply.github.com>
2023-12-27 12:30:14 +0530

    Update BTsetdepth.java

--- exercises/BTsetdepth.java
+++ exercises/BTsetdepth.java
@@ -1,3 +1,5 @@
+//Write a recursive function to set the value for each node in a binary tree to be its depth then return the modified tree. 
+//Assume that nodes store integer values. On the initial call to BTsetdepth, depth is 0.
 public BinNode BTsetdepth(BinNode root, int depth)
 {
 	if(root!=null){
