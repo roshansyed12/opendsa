1a009738c5b6ef8470447ed4f10bbca872ad043d
roshansyed12 <154960598+roshansyed12@users.noreply.github.com>
2023-12-28 11:31:46 +0530

    Create MBTmirror.java

---
+++ exercises/MBTmirror.java
@@ -0,0 +1,13 @@
+//Given two binary trees, return true if and only if they are mirror images of each other. Note that two empty trees are considered mirror images.
+public  boolean MBTmirror(BinNode root1, BinNode root2)
+{
+    if(root1==null && root2==null) return true;
+    if((root1==null && root2!=null) ||
+        (root1!=null && root2==null) ||
+      		root1.value()!=root2.value()) return false;
+    else{
+        MBTmirror(root1.left(),root2.left());
+        MBTmirror(root1.right(),root2.right());
+        return true;
+    }
+}
