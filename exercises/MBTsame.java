1e12fb6a9ea2dfa8c8e38f1dcec6f459831f253f
roshansyed12 <154960598+roshansyed12@users.noreply.github.com>
2023-12-28 11:41:31 +0530

    Create MBTsame.java

---
+++ exercises/MBTsame.java
@@ -0,0 +1,13 @@
+//Given two binary trees, return true if they are identical (they have nodes with the same values, arranged in the same way).
+public  boolean MBTsame(BinNode root1, BinNode root2)
+{
+    if(root1==null && root2==null) return true;
+    if((root1==null && root2!=null) ||
+        (root1!=null && root2==null) ||
+      		root1.value()!=root2.value()) return false;
+    else{
+        MBTsame(root1.left(),root2.left());
+        MBTsame(root1.right(),root2.right());
+        return true;
+    }
+}
